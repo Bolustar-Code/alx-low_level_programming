c9c833666657d6bd7448d4a7e4cfbb84e1c8ae50
Ipinsokansunday <ipinboluwaji@yahoo.com>
2023-09-12 13:58:52 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - check if the given character is lower case.
+ * @r: character to check if within the ASCII range of lowercase letters.
+ * Return: 1 (True) else 0 (False)
+ */
+int _islower(char r)
+{
+	if (r >= 'a' && r <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
