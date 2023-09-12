0b7bc442063848c36dd1613cdb3fd233bcefff1f
Ipinsokansunday <ipinboluwaji@yahoo.com>
2023-09-12 14:05:47 -0700

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * @r: character to check if within the ASCII range of lowercase letters.
  * Return: 1 (True) else 0 (False)
  */
-int _islower(char r)
+int _islower(int r)
 {
 	if (r >= 'a' && r <= 'z')
 	{
