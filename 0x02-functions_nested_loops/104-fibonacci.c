3e3576078991587b5e3adcec8007615b65b271ea
Ipinsokansunday <ipinboluwaji@yahoo.com>
2023-09-13 04:06:51 -0700

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,44 +1,46 @@
 #include <stdio.h>
 /**
- * print_fib - prints the first 50 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line.
- * @len: the length of with the fib sequance should print to.
- *
- * Return: void
- */
-void print_fib(int len)
-{
-	unsigned int target, i;
-
-	unsigned int former_l, former_r,;
-	unsigned int latter_l, latter_r;
-	unsigned int fib_l, fib_r;
-
-	f = 1;
-	l = 2;
-	target = len - 2;
-	for (i = 0 ; i < target ; i++)
-	{
-		fib = f + l;
-		printf("%d : ", i + 4);
-		printf("%.0Lf" , fib);
-		f = l;
-		l = fib;
-		if (i < target - 1)
-			printf("\n");
-	}
-	putchar('\n');
-}
-
-
-/**
- * main - Entry point,
- *
- *
- * Return: Always 0 (sucess)
- */
+*main - finds and prints the first 98 Fibonacci numbers, from with 1 and 2
+*fibonacci suit numbers
+*Return: each time 0
+*/
 int main(void)
 {
-	print_fib(98);
+	int fibn;
+	unsigned long f1 = 0, f2 = 1, f3;
+	unsigned long f1_s1, f1_s2, f2_s1, f2_s2;
+	unsigned long s1, s2;
+
+	for (inc = 0; inc < 92; inc++)
+	{
+		f3 = f1 + f2;
+		printf("%lu, ", f3);
+		f1 = f2;
+		f2 = f3;
+	}
+	f1_s1 = s1 / 10000000000;
+	f2_s1 = f2 / 10000000000;
+	f1_s2 = f1 % 10000000000;
+	n2_h2 = n2 % 10000000000;
+
+	for (fibn = 93; fibn < 99; fibn++)
+	{
+		s1 = f1_s1 + f2_s1;
+		s2 = f1_s2 + f2_s2;
+		if ((f1_s2 + f2_s2) > 9999999999)
+		{
+			s1 += 1;
+			s2 %= 10000000000;
+		}
+		printf("%lu%lu", s1, s2);
+		if (fibn != 98)
+		printf(", ");
+
+		f1_s1 = f2_s1;
+		f1_s2 = f2_s2;
+		f2_s1 = s1;
+		f2_s2 = s2;
+	}
+	printf("\n");
 	return (0);
 }
