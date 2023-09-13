1b7e39136c27563794c5f9d84cbf14668fd5fdf0
Ipinsokansunday <ipinboluwaji@yahoo.com>
2023-09-13 01:50:34 -0700

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,119 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_thousand - print out the digit in the thousand spot
- * @sum: Value to scan for thousand spot
- *
- * Return: Void
- */
-void print_times_table(int n);
-static void print_thousand(int sum)
-{
-	if (sum / 1000 == 0)
-	{
-		putchar(32);
-	}
-	else
-	{
-		putchar('0' + (sum / 1000));
-	}
-}
-
-
-/**
- * print_hundred - print out the digit in the hundred spot
- * @sum: Value to scan for hundred spot
- *
- * Return: Void
- */
-static void print_hundred(int sum)
-{
-	if (sum / 100 == 0)
-	{
-		putchar(32);
-	}
-	else
-	{
-		putchar('0' + (sum / 100));
-	}
-}
-
-
-/**
- * print_tenth - print out the digit in the tenth spot
- * @sum: Value to scan for tenth spot
- *
- * Return: Void
- */
-static void print_tenth(int sum)
-{
-	if (sum / 10 == 0)
-	{
-		putchar(32);
-	}
-	else
-	{
-		if ((sum / 10) > 9)
-		{
-			putchar('0' + (sum / 10 % 10));
-		}
-		else
-		{
-			putchar('0' + (sum / 10));
-		}
-	}
-}
-
-
-/**
- * print_base10 - print out the digit in the base10 spot
- * @sum: Value to scan for base10 spot
- *
- * Return: Void
- */
-static void print_base10(int sum)
-{
-	if (sum % 10 == 0)
-	{
-		putchar('0');
-	}
-	else
-	{
-		putchar('0' + (sum % 10));
-	}
-}
-
-/**
- * print_times_table - print out the 9 times table
- * @n: Valuse use to generate times tables.
- *
- * Return: void
- */
+*print_times_table - prints the  times table with
+*parameter
+*@n: parameter for the multiplication table
+*Return: returns always 0
+*/
 void print_times_table(int n)
 {
-	int v, h;
+	int x, y, z;
 
-	if (n > 15 || n < 0)
+	if (n <= 15 && n >= 0)
 	{
-		return;
-	}
-	for (h = 0 ; h <= n ; h++)
-	{
-		for (v = 0 ; v <= n ; v++)
+		for (x = 0; x <= n; x++)
 		{
-			int sum = v * h;
+			_putchar('0');
 
-			if (v != 0)
+			for (y = 1; y <= n; y++)
 			{
-				putchar(',');
-				putchar(' ');
+				_putchar(',');
+				_putchar(' ');
+				z = x * y;
+
+				if (z <= 99)
+				_putchar(' ');
+
+				if (z <= 9)
+				_putchar(' ');
+
+				if (z >= 100)
+				{
+					_putchar((z / 100) + '0');
+					_putchar((z / 10) % 10 + '0');
+				}
+				else if (z <= 99 && z >= 10)
+				{
+					_putchar((z / 10) + '0');
+				}
+					_putchar((z % 10) + '0');
 			}
-			print_thousand(sum);
-			print_hundred(sum);
-			print_tenth(sum);
-			print_base10(sum);
+			_putchar('\n');
 		}
-		putchar('\n');
 	}
 }
