747fe099211baa03434ba8d8ff88093532bbe4f6
Ipinsokansunday <ipinboluwaji@yahoo.com>
2023-09-13 01:15:52 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,11 +12,11 @@ static void print_thousand(int sum)
 {
 	if (sum / 1000 == 0)
 	{
-		_putchar(32);
+		putchar(32);
 	}
 	else
 	{
-		_putchar('0' + (sum / 1000));
+		putchar('0' + (sum / 1000));
 	}
 }
 
@@ -31,11 +31,11 @@ static void print_hundred(int sum)
 {
 	if (sum / 100 == 0)
 	{
-		_putchar(32);
+		putchar(32);
 	}
 	else
 	{
-		_putchar('0' + (sum / 100));
+		putchar('0' + (sum / 100));
 	}
 }
 
@@ -50,17 +50,17 @@ static void print_tenth(int sum)
 {
 	if (sum / 10 == 0)
 	{
-		_putchar(32);
+		putchar(32);
 	}
 	else
 	{
 		if ((sum / 10) > 9)
 		{
-			_putchar('0' + (sum / 10 % 10));
+			putchar('0' + (sum / 10 % 10));
 		}
 		else
 		{
-			_putchar('0' + (sum / 10));
+			putchar('0' + (sum / 10));
 		}
 	}
 }
@@ -76,11 +76,11 @@ static void print_base10(int sum)
 {
 	if (sum % 10 == 0)
 	{
-		_putchar('0');
+		putchar('0');
 	}
 	else
 	{
-		_putchar('0' + (sum % 10));
+		putchar('0' + (sum % 10));
 	}
 }
 
@@ -106,14 +106,14 @@ void print_times_table(int n)
 
 			if (v != 0)
 			{
-				_putchar(',');
-				_putchar(' ');
+				putchar(',');
+				putchar(' ');
 			}
 			print_thousand(sum);
 			print_hundred(sum);
 			print_tenth(sum);
 			print_base10(sum);
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
