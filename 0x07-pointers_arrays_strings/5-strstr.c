844ac9cfd6db59827c85d31f59a3e12d6aabb2a6
Ipinsokansunday <ipinboluwaji@yahoo.com>
2023-09-25 09:37:30 -0700

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - Locates a substring within a string.
+ * @haystack: The string to search within.
+ * @needle: The substring to locate.
+ * Return: A pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (haystack);
+	}
+	return (0);
+}
