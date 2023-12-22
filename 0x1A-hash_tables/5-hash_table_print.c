998cbaa88d9c38a5c33c6695b372740e93e05c9e
Ipinsokansunday <ipinboluwaji@yahoo.com>
2023-12-22 09:38:00 -0800

    5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - Prints the contents of a hash table.
+ * @ht: The hash table to print.
+ *
+ * Return: Void.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	int printed = 0;
+
+	if (ht == NULL)
+	return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			if (printed)
+			printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			printed = 1;
+			current = current->next;
+		}
+	}
+	printf("}\n");
+}
