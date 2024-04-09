ceb291fea2592574bdb9e53420cf989a6f7c2368
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-04-09 04:13:33 -0700

    1. Binary search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,13 +1,14 @@
 #include "search_algos.h"
-
 /**
- * linear_search - searches for a value in an array of
- * integers using the Linear search algorithm
+ * linear_search - Searches for a value in an array of integers using
+ *                 the Linear search algorithm.
  *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: Always EXIT_SUCCESS
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: The first index where @value is located, or -1 if @value
+ *         is not present in @array or if @array is NULL.
  */
 
 
